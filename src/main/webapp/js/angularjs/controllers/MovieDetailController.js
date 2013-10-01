74d7e525679227acef348025848b2c048bd1ef52
Enrique <engapa@gmail.com>
2013-10-01 10:30:50 +0200

    Decrease index value in order to sync json id

--- src/main/webapp/js/angularjs/controllers/MovieDetailController.js
+++ src/main/webapp/js/angularjs/controllers/MovieDetailController.js
@@ -9,7 +9,7 @@ function MovieDetailController($scope, $http, $routeParams) {
   $scope.movieId = parseInt($routeParams.movieId);
   
   $http.get('data/movies.json').success(function(data) {
-	  $scope.movie = data[$scope.movieId + 1];
+	  $scope.movie = data[$scope.movieId - 1];
   });
  
 }
