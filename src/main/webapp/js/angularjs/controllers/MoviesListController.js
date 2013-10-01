e8330a5a2a294fc46ede5d54906bdd2463e90c72
Enrique <engapa@gmail.com>
2013-10-01 09:43:46 +0200

    Label with version of AngularJS

--- src/main/webapp/js/angularjs/controllers/MoviesListController.js
+++ src/main/webapp/js/angularjs/controllers/MoviesListController.js
@@ -6,6 +6,10 @@ function MoviesListController($scope, $http) {
   $http.get('data/movies.json').success(function(data) {
 	$scope.movies = data;
   });
+  
+  $http.get('lib/version.txt').success(function(data) {
+		$scope.angular_version = data;
+  });
    
   //Combo box for ordering movies
   $scope.orderField = "title";
